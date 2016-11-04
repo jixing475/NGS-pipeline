ece4d667f1210363ef08ac7069f3d62122904d2e
jixing475 <jixing475@gmail.com>
2016-11-04 16:24:57 +0800

    Update 0_fastqc.sh

--- WES/0_fastqc.sh
+++ WES/0_fastqc.sh
@@ -1,5 +1,5 @@
 #/zzh_gpfs02/jixing/DataBase/bingli_dingyanqing/Exome/first
 for id in `find  /zzh_gpfs02/jixing/DataBase/bingli_dingyanqing/Exome/first -name "*.fq.gz"`;
 do
-bsub -n 4 -q cpu -e err_%J -o out_%J "/zzh_gpfs/apps/FastQC/fastqc $i -o /zzh_gpfs02/jixing/NTL/20161104_Exome/fastQC/" ;
+bsub -n 4 -q cpu -e err_%J -o out_%J "/zzh_gpfs/apps/FastQC/fastqc $id -o /zzh_gpfs02/jixing/NTL/20161104_Exome/fastQC/" ;
 done;
