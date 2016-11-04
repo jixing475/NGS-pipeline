b62f4a1d082b28e6061446200aed7bfb1aa311f5
jixing475 <jixing475@gmail.com>
2016-11-04 18:40:26 +0800

    Update 01_aligment_bwa.sh

--- WES/01_aligment_bwa.sh
+++ WES/01_aligment_bwa.sh
@@ -6,5 +6,5 @@ DATA=/zzh_gpfs02/jixing/DataBase/bingli_dingyanqing/Exome
 for SAMPLE_ID in  193-LB 193-NB 193-TB 1-LB 1-NB 1-TB 215-LB 215-NB 215-TB 40A-LB 40A-NB 40A-TB 4-LB 4-NB 4-TB 5-LB 5-NB 5-TB
 do
 bsub -n 8 -q cpu -e err.bwa_%J -o out.bwa_%J \
-"$BWA mem -M -t 8 $REFERENCE $({DATA}/first/${SAMPLE_ID}_1.clean.fq.gz $({DATA}/first/${SAMPLE_ID}_2.clean.fq.gz > ${work_dir}/${SAMPLE_ID}.sam";
+"$BWA mem -M -t 8 $REFERENCE ${DATA}/first/${SAMPLE_ID}_1.clean.fq.gz ${DATA}/first/${SAMPLE_ID}_2.clean.fq.gz > ${work_dir}/${SAMPLE_ID}.sam";
 done
